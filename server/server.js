d05a9b9864c236b639a673ca7e644c371387d00d
August Dolan <august.dolan1@gmail.com>
2020-09-28 10:42:58 -0700

    Set up spec files, install jest

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const db = require('../db/index.js')
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/products/find', (req, res) => {
   db.find({})
     .then((results) => {
       res.send(results);

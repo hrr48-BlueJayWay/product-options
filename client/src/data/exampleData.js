78cca4734110df82b4fc2369d47e867fd9b84693
August Dolan <august.dolan1@gmail.com>
2020-09-29 16:10:33 -0700

    Update data to include sales price

--- client/src/data/exampleData.js
+++ client/src/data/exampleData.js
@@ -1,88 +1,99 @@
-const data = {
-  "price": {
-      "originalPrice": 915,
-      "salePrice": 915
-  },
-  "colors": [
-      "tan",
-      "plum",
-      "teal"
-  ],
-  "sizes": [
-    "small",
-    "medium",
-    "large",
-  ],
-  "_id": "5f725185cf20242ed2264ebc",
-  "liked": false,
-  "inStock": 12973,
-  "reviews": [
-      {
-          "_id": "5f725185cf20242ed2264ebd",
-          "overallRating": 1,
-          "easeOfAssembly": 2,
-          "valueForMoney": 3,
-          "productQuality": 3,
-          "appearance": 3,
-          "worksAsExpected": 5,
-          "createdAt": "2020-05-08T15:57:02.208Z",
-          "iRecommendThisProduct": false,
-          "header": "possimus aspernatur possimus",
-          "body": "Voluptatem a reprehenderit soluta. Alias est deserunt nobis perferendis. Iste quasi eaque rerum nihil commodi. Voluptas aut rerum porro impedit est velit animi et in.\n \rOmnis sed et dolor a est. Expedita sunt sunt nam. Rerum id amet labore pariatur.\n \rTempora voluptatem sed magni soluta possimus. Debitis beatae aut amet qui quo. Deleniti cupiditate minima provident ut aut inventore laborum commodi. Consequuntur ex ipsam neque veritatis illum voluptatem ut quo."
-      },
-      {
-          "_id": "5f725185cf20242ed2264ebe",
-          "overallRating": 5,
-          "easeOfAssembly": 3,
-          "valueForMoney": 4,
-          "productQuality": 2,
-          "appearance": 1,
-          "worksAsExpected": 5,
-          "createdAt": "2020-07-23T05:06:19.473Z",
-          "iRecommendThisProduct": true,
-          "header": "voluptates veritatis ut",
-          "body": "Molestias suscipit necessitatibus. Eos quia sed doloribus porro ex mollitia illum explicabo. Tenetur eveniet quam unde perspiciatis quis ipsum qui voluptas.\n \rAut placeat officia laboriosam voluptatum voluptatum laborum. Doloribus labore adipisci fuga dicta fuga ut nihil. Vero quas et omnis. Doloremque placeat omnis voluptatum dolorem sit dolores ipsam autem. Ad ut quam odit dolorem sed quos numquam accusantium.\n \rConsequatur sit qui assumenda. Eum molestiae fuga quis aut. Accusantium ipsa tenetur pariatur tempora nesciunt. Beatae autem aspernatur nesciunt rerum autem nulla at consequatur ab. Non quis incidunt cupiditate et eos laborum."
-      },
-      {
-          "_id": "5f725185cf20242ed2264ebf",
-          "overallRating": 5,
-          "easeOfAssembly": 2,
-          "valueForMoney": 3,
-          "productQuality": 1,
-          "appearance": 5,
-          "worksAsExpected": 1,
-          "createdAt": "2020-01-27T13:38:19.263Z",
-          "iRecommendThisProduct": true,
-          "header": "in tempore nobis",
-          "body": "Quas necessitatibus placeat. Voluptas voluptates nisi. Omnis dicta alias dicta dolorem eum nobis blanditiis molestiae consequatur. Blanditiis dolor alias ratione repellat consequatur atque sed. Provident sit minus. Officia repudiandae dolorum ipsa quasi incidunt cupiditate numquam.\n \rNeque explicabo consequuntur dignissimos sint. Repellat in consequatur aut facilis est magni. Repellat quaerat rem enim praesentium ipsum aliquam explicabo quia. Ut aut neque illo.\n \rA ea eos voluptas corrupti dolores consequuntur. Omnis velit odio veniam dolorem ex odio et vel et. Fugit sed et quae ut voluptatem. Ad modi impedit sit ut aut atque. Rerum velit quia quae quis quia soluta asperiores. Aliquid dolorem ut facere."
-      },
-      {
-          "_id": "5f725185cf20242ed2264ec0",
-          "overallRating": 3,
-          "easeOfAssembly": 1,
-          "valueForMoney": 4,
-          "productQuality": 3,
-          "appearance": 4,
-          "worksAsExpected": 5,
-          "createdAt": "2020-04-05T12:57:50.802Z",
-          "iRecommendThisProduct": true,
-          "header": "aut sit beatae",
-          "body": "Quisquam quo veritatis et vel voluptates. Nihil distinctio quia earum aperiam nam rerum quisquam at. Ducimus voluptatem ea temporibus placeat amet reiciendis hic.\n \rHarum sit non praesentium. Dignissimos qui reiciendis quae necessitatibus earum in consequatur iusto. Nam quae aut error commodi commodi. Quia temporibus ea. Sit nihil nobis voluptatem sint.\n \rQui explicabo ea doloremque dolor enim vitae quis sed enim. Ab quod laborum cum quidem qui maiores quia. Ipsa perspiciatis corrupti maxime eum sunt enim. Ipsam nostrum voluptates error aut dolorum aperiam optio veniam mollitia. Assumenda consequatur id dolores. Perspiciatis dolor dolorum aut quam."
-      },
-      {
-          "_id": "5f72564819629738b9056f6c",
-          "easeOfAssembly": 3,
-          "valueForMoney": 3,
-          "productQuality": 3,
-          "appearance": 3,
-          "worksAsExpected": 3,
-          "header": "test",
-          "body": "test",
-          "iRecommendThisProduct": true
-      }
-  ],
-  "id": "45",
-  "__v": 1
-}
+const exampleData = {
+    "price": {
+        "originalPrice": 528,
+        "salePrice": 300
+    },
+    "colors": [
+        "orange"
+    ],
+    "sizes": [],
+    "_id": "5f7397b36d78c4180781a9c3",
+    "title": "corrupti",
+    "description": "Nostrum fuga quod corrupti velit praesentium rerum earum eligendi nostrum.",
+    "liked": false,
+    "inStock": 11435,
+    "reviews": [
+        {
+            "_id": "5f7397b36d78c4180781a9c4",
+            "overallRating": 3,
+            "easeOfAssembly": 2,
+            "valueForMoney": 5,
+            "productQuality": 4,
+            "appearance": 5,
+            "worksAsExpected": 4,
+            "createdAt": "2020-08-13T02:59:36.711Z",
+            "iRecommendThisProduct": true,
+            "header": "dolores autem debitis",
+            "body": "Laudantium iste quibusdam quisquam animi assumenda voluptatibus esse minima assumenda. Eum nobis iusto. Officia inventore et qui facere quia.\n \rQui natus qui repellat perferendis delectus ea neque dolorem aspernatur. A dolores nihil. Omnis accusamus aperiam et asperiores blanditiis voluptatem voluptatem aut. Ea dolorum ducimus voluptas sed aut inventore. Quo dolorem rerum qui voluptate doloremque perferendis nemo molestias velit. Recusandae qui tempore rerum ut eveniet deserunt.\n \rQui quia ut repudiandae. Officia quas consequatur occaecati eos cumque sint. Deserunt aut sapiente cumque. Natus laudantium adipisci optio veniam tempore. Quibusdam ex adipisci. Et ducimus nulla qui quas."
+        },
+        {
+            "_id": "5f7397b36d78c4180781a9c5",
+            "overallRating": 2,
+            "easeOfAssembly": 1,
+            "valueForMoney": 4,
+            "productQuality": 3,
+            "appearance": 4,
+            "worksAsExpected": 5,
+            "createdAt": "2020-08-14T18:01:21.442Z",
+            "iRecommendThisProduct": false,
+            "header": "vel exercitationem eos",
+            "body": "Ducimus necessitatibus voluptatem atque a asperiores. Labore repudiandae minima earum et laudantium rerum est. Qui aliquam nesciunt dignissimos molestiae est. Voluptates sunt est nihil sint numquam a accusantium enim. Maxime vitae quia dignissimos omnis officia qui et ut cumque.\n \rFacilis est exercitationem aut ut debitis quidem quam velit vitae. Blanditiis numquam omnis qui. Tempora nesciunt explicabo unde illum amet incidunt in sint.\n \rEst deleniti temporibus omnis corrupti dolor voluptatem. Est eligendi tenetur minima sequi reiciendis dicta. Exercitationem hic vel nesciunt nam et rem officiis. Molestias aut ipsam."
+        },
+        {
+            "_id": "5f7397b36d78c4180781a9c6",
+            "overallRating": 2,
+            "easeOfAssembly": 5,
+            "valueForMoney": 3,
+            "productQuality": 1,
+            "appearance": 3,
+            "worksAsExpected": 5,
+            "createdAt": "2019-11-30T14:20:12.076Z",
+            "iRecommendThisProduct": true,
+            "header": "eos ullam repudiandae",
+            "body": "Odio quasi et corporis eligendi aut doloremque maiores voluptates quia. Molestiae eos dicta aut sit facere. Iusto itaque quia facilis consequuntur delectus corrupti placeat voluptatem quas. Qui officia eius dolore sit dolore molestias doloremque ipsam.\n \rPlaceat qui culpa ut quis voluptatem est. Ut qui est quia et voluptatibus. Porro nisi et similique corporis qui ex rerum corrupti possimus. Minima veniam voluptates excepturi debitis quod quae.\n \rNatus occaecati voluptas facilis architecto dolore sint. Quod necessitatibus non eligendi consequatur. Dicta incidunt veritatis sapiente. Dolorum debitis possimus. Vel facere quia consequatur consequatur velit voluptate dolorem architecto."
+        },
+        {
+            "_id": "5f7397b36d78c4180781a9c7",
+            "overallRating": 3,
+            "easeOfAssembly": 5,
+            "valueForMoney": 5,
+            "productQuality": 2,
+            "appearance": 4,
+            "worksAsExpected": 1,
+            "createdAt": "2020-03-13T20:38:49.276Z",
+            "iRecommendThisProduct": false,
+            "header": "maiores atque impedit",
+            "body": "Quod veniam quam ut ut suscipit harum eaque quas nemo. Debitis vitae vel itaque repudiandae modi vel. Doloribus qui error magnam ut sit et cumque dolorem.\n \rUnde at veritatis quam rem aut assumenda quam ex doloribus. Cumque minima vel ex quibusdam laudantium. Explicabo quas quod quam voluptatum.\n \rRerum tenetur voluptatibus sapiente quos amet. Iste voluptatem voluptatibus voluptatem dolore rerum sunt soluta quis. Voluptatem sed numquam recusandae consequatur quo animi sint eius illo. Aut ratione et repellendus consequatur pariatur qui quos quo nihil."
+        },
+        {
+            "_id": "5f7397b36d78c4180781a9c8",
+            "overallRating": 5,
+            "easeOfAssembly": 4,
+            "valueForMoney": 2,
+            "productQuality": 3,
+            "appearance": 2,
+            "worksAsExpected": 4,
+            "createdAt": "2020-03-11T05:54:43.545Z",
+            "iRecommendThisProduct": false,
+            "header": "voluptas numquam totam",
+            "body": "Deserunt ea vero. Quidem distinctio et qui et molestiae itaque dolor at. Dolor exercitationem autem veniam. Velit aliquid eos nihil voluptatum. Et sit omnis nam vel.\n \rNisi et incidunt repellat voluptas voluptatem quo ipsam. Ut iusto aperiam a aspernatur eaque. Exercitationem nihil qui eaque quas id molestias iusto.\n \rIllo veritatis velit rem omnis. Vel quos quo quaerat voluptates exercitationem iusto eos iusto. Ut iure ut sapiente maiores ratione natus corporis. Ullam error mollitia quidem accusamus at fugit enim repudiandae. Eligendi sed aperiam rerum."
+        },
+        {
+            "_id": "5f7397b36d78c4180781a9c9",
+            "overallRating": 1,
+            "easeOfAssembly": 4,
+            "valueForMoney": 3,
+            "productQuality": 5,
+            "appearance": 2,
+            "worksAsExpected": 5,
+            "createdAt": "2020-08-03T05:10:40.869Z",
+            "iRecommendThisProduct": false,
+            "header": "consequuntur voluptatum nihil",
+            "body": "Qui animi et et eos consectetur quas rerum. Et nihil dolor odio voluptate. Delectus aut et adipisci. Ut eaque dolor et dolores alias dolorem distinctio.\n \rQuam qui et incidunt molestias cumque voluptas. Quae sit occaecati eum accusantium distinctio. Quidem at omnis. Suscipit voluptatum assumenda quibusdam tempore earum sit. Esse recusandae commodi quas eligendi consequatur minus ad.\n \rQuia nam error ut quas molestiae ut iusto. Libero itaque ex voluptatum provident velit quis ab. Rerum deserunt et iusto voluptate iusto earum excepturi illo amet. Modi officia id veniam."
+        }
+    ],
+    "id": "45",
+    "__v": 0
+    }
 
-export default data;
+export default exampleData;

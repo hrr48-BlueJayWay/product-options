4a16014699c08d0a65db045009e629284fd677bd
August Dolan <august.dolan1@gmail.com>
2020-09-30 08:52:30 -0700

    Set up global style

--- client/src/components/ProductOverview.jsx
+++ client/src/components/ProductOverview.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const ProductOverview = (props) => {
   const { data } = props;
   let isOnSale = data.price.salePrice < data.price.originalPrice
 
+  return (
   <div>
     <div id="title">
       <h1>{data.title}</h1>
       <p>{data.description}</p>
     </div>
+
     <div id="price">
       <div>{data.price.originalPrice}</div>
       {isOnSale && <div>{data.price.salePrice}</div>}
     </div>
     <div id="reviews-module">stars svg and review num here</div>
   </div>
+
+  );
 };
 
 export default ProductOverview;

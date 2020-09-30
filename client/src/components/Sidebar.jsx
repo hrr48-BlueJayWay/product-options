f928e0edb9fba25799dbb5e5dd660310a4f75bf6
August Dolan <august.dolan1@gmail.com>
2020-09-30 15:22:26 -0700

    Begin Sidebar stencil

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Main = styled.div`
+  width: 75%;
+  height: 100%;
+  background-color: #000;
+`;
+
+const Sidebar = () => {
+
+  return (
+    <Main>
+      <h1>Hello world</h1>
+    </Main>
+  )
+}
+
+export default Sidebar;

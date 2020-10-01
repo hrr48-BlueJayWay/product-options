6e51ae7fabefa2c9095f8b523bff0305e0a01b54
August Dolan <august.dolan1@gmail.com>
2020-10-01 06:53:35 -0700

    Create dynamic module render; hello world Reviews

---
+++ client/src/components/Reviews.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Reviews = (props) => {
+  console.log(props.data);
+  return (
+    <h1>HELLO MY DELICIOUS WORLD</h1>
+  );
+};
+
+export default Reviews;

5bd8248f0a699d2e0baa1b9d51598b114157b0b0
August Dolan <august.dolan1@gmail.com>
2020-10-02 08:47:41 -0700

    add flex, arrow, and child style on parent hover

--- client/src/components/VarianceOverview.jsx
+++ client/src/components/VarianceOverview.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
-const Title = styled.div`
+const FlexContainer = styled.div`
   border-top: 1px solid #f5f5f5;
   padding-top: 1.5rem;
+  display: flex;
+  justify-content: space-between;
+  cursor: pointer;
+`;
+
+const Title = styled.div`
   font-weight: 700;
   font-size: 0.9rem;
+  ${FlexContainer}:hover & {
+    text-decoration: underline;
+  }
 `;
 
 const SelectedOption = styled.div`
@@ -15,15 +24,30 @@ const SelectedOption = styled.div`
   margin-bottom: 1.5rem;
 `;
 
+const Icon = styled.svg`
+  flex: none;
+  transition: fill 0.25s;
+  width: 1.5rem;
+  height: 1.5rem;
+`;
+
+const Arrow =
+  <Icon viewBox="0 0 24 24">
+    <path fillRule="evenodd" clipRule="evenodd" d="M15.5996 11.9999L9.81456 17.7857L8.40026 16.3716L12.7714 11.9999L8.40026 7.62823L9.81457 6.21411L15.5996 11.9999Z"></path>
+  </Icon>;
+
 const VarianceOverview = (props) => {
   console.log(props);
   const [currentOption, setCurrentOption] = useState(props.options[0])
 
   return (
-    <div>
-      <Title>Color</Title>
-      <SelectedOption>{currentOption}</SelectedOption>
-    </div>
+    <FlexContainer>
+      <div>
+        <Title>Color</Title>
+        <SelectedOption>{currentOption}</SelectedOption>
+      </div>
+      {Arrow}
+    </FlexContainer>
   );
 };
 

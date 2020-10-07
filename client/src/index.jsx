52de02388291cf4c9a70650bfc059f58dffafb85
August Dolan <august.dolan1@gmail.com>
2020-10-06 19:12:03 -0700

    hook front end up to data via /products/:id

---
+++ client/src/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, useParams, Redirect } from 'react-router-dom';
+import axios from 'axios';
+import App from './app.jsx';
+
+
+
+
+ReactDOM.render(
+<Router>
+  <Route exact path="/">
+    <Redirect to="/products/1" />
+  </Route>
+  <Route path="/products/:id">
+    <App />
+  </Route>
+</Router>, document.getElementById("app"));
